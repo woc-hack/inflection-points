949a765d75b21695caea74262737fe2ef77b6c6b
Noah Burgin <noah22@vols.utk.edu>
2020-12-26 18:27:34 -0500

    improved data production scripts and updated for version S

--- scripts/data_to_timeseries.sh
+++ scripts/data_to_timeseries.sh
@@ -9,15 +9,27 @@
 N=30
 
 (
-cat missing_projects_timestamp-csvs | while read a
+# projects missed in the first run
+#cat missing_projects_timestamp-csvs | while read a
+
+# all projects from R
 #sort -n -t',' -k2 /data/play/inflection_points/data_R_8311.csv | sed '1,4d' | cut -d',' -f1 | while read a
+
+# project names from S
+cat /home/noah22/inflection-points/project_names_S.txt | while read a
+
+# small set of projects for testing
 #sort -nr -t',' -k2 /data/play/inflection_points/data_R_8311.csv | tail -8 | head -2 | cut -d',' -f1 | while read a
+
+# re-compute empty files
+#wc -l /data/play/inflection_points/auth-and-comm_data/* | awk '$1 == 0' | cut -d '/' -f6 | sed 's/\.csv$//' | while read a
+
 do
 	((i=i%N)); ((i++==0)) && wait
-	echo $a | ~/lookup/getValues -f p2c | cut -d';' -f2 | ~/lookup/getValues -f c2ta | cut -d';' -f2 | awk 'NR % 2' | /home/noah22/python3.9/Python-3.9.0/python timestamps_to_series.py > /data/play/inflection_points/new_timeseries_data/$a.csv &
+	b=$(echo "$a" | sed 's/\//_/g')
+	echo $a | ~/lookup/getValues -f p2c | cut -d';' -f2 | ~/lookup/getValues c2ta 2>/dev/null | cut -d';' -f2-3 | \
+	python3.9 /home/noah22/inflection-points/auth-comm_proj_report.py > /da0_data/play/inflection_points/auth-and-comm_data_S/$b.csv &
 done
 )
 
 wait
-
-#head -2 /data/play/inflection_points/data_R_8311.csv | sed 1d | cut -d',' -f1 | while read a
