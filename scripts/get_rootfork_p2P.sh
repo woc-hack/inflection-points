ae54365e43adc69450a98ab24f40f39c00bf7b5b
Noah Burgin <noah22@vols.utk.edu>
2021-01-03 15:52:15 -0500

    added the other rootfork script

---
+++ scripts/get_rootfork_p2P.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Usage: ./get_rootfork.sh p_project_name
+# Print rootfork field of a project or the project name if it is root
+# Provided project does NOT have to be P from WoC's p2P
+
+if [ -z "$1" ]
+then
+	echo "usage: get_rootfork.sh P_project_name"
+	exit
+fi
+
+# Get WoC root project and protect against ssh errors (try until success)
+c=""
+while [ -z "$c" ]
+do
+	#echo "Trying p2P for $1"
+	c=$(echo $1 | ~/lookup/getValues -f p2P 2>/dev/null 2>/dev/null | cut -d';' -f2)
+done
+
+# Search mongo for rootfork
+out=$(mongoexport --quiet --host "da1.eecs.utk.edu" --db WoC --collection \
+'proj_metadata.S' --query "{projectID: "\""$c"\""}" -f 'rootFork' --type=csv \
+--noHeaderLine)
+
+# Print rootfork or provided project name if mongo returned nothing (means it's root)
+printf "$1;"
+if [[ -z $out ]]; then echo $1; else echo $out; fi
