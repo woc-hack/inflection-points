714ce6b6c798dcfb5239368aaee41207400c311e
Noah Burgin <noah22@vols.utk.edu>
2021-01-03 14:34:33 -0500

    improved get_rootfork script

--- scripts/get_rootfork.sh
+++ scripts/get_rootfork.sh
@@ -3,10 +3,25 @@
 # Usage: ./get_rootfork.sh project_name
 # Print rootfork field of a project or the project name if it is root
 
-out=$(mongoexport --quiet --host "da1.eecs.utk.edu" --db WoC --collection 'proj_metadata.S' --query "{projectID: "\""$1"\""}" -f 'rootFork' --type=csv --noHeaderLine)
+if [ -z "$1" ]
+then
+	echo "usage: get_rootfork.sh project_name"
+	exit
+fi
 
-# Use to test if project is rootfork or not (test existence of output in pipe series)
-#echo "$out"
+# Get WoC root project and protect against ssh errors (try until success)
+c=""
+while [ -z "$c" ]
+do
+	#echo "Trying p2P for $1"
+	c=$(echo $1 | ~/lookup/getValues -f p2P 2>/dev/null 2>/dev/null | cut -d';' -f2)
+done
 
-# Use to either print rootfork or original project name
+# Search mongo for rootfork
+out=$(mongoexport --quiet --host "da1.eecs.utk.edu" --db WoC --collection \
+'proj_metadata.S' --query "{projectID: "\""$c"\""}" -f 'rootFork' --type=csv \
+--noHeaderLine)
+
+# Print rootfork or provided project name if mongo returned nothing (means it's root)
+printf "$1;"
 if [[ -z $out ]]; then echo $1; else echo $out; fi
