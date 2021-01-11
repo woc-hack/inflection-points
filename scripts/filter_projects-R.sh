e96b2928d41e4ab123f3a46f1807100057357973
Noah Burgin <noah22@vols.utk.edu>
2021-01-11 15:43:36 -0500

    added filter_projects-R and renamed S's to distinguish

---
+++ scripts/filter_projects-R.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# usage: filter_projects.sh mongoexport_file
+# example: filter_projects.sh /da1_data/play/inflection-points/mongo-exports/mongo_proj_metadata_R_out.csv 
+
+awk -F',' '($4 > 0 && $6 - $4 > 126144000) || NR==1' $1
