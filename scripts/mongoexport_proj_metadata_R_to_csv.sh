312f6eecc03cc400404b4fc37323e68e83935d17
Noah Burgin <noah22@vols.utk.edu>
2021-01-11 15:50:04 -0500

    minor tweaks to existing scripts

--- scripts/mongoexport_proj_metadata_R_to_csv.sh
+++ scripts/mongoexport_proj_metadata_R_to_csv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Export a MongoDB query to a csv file. The desired fields are in ~/inflection-points/mongo-R_fieldsarg.txt
-mongoexport --host "da1.eecs.utk.edu" --db WoC --collection 'proj_metadata.R' --query '{NumAuthors: {$gt:50}, NumCommits: {$gt:5000}}' --type=csv --fieldFile ~/inflection-points/mongo-R_fieldsarg.txt --out out.csv
+mongoexport --host "da1.eecs.utk.edu" --db WoC --collection 'proj_metadata.R' --query '{NumAuthors: {$gt:50}, NumCommits: {$gt:5000}}' --type=csv --fieldFile ~/inflection-points/mongo-R_fieldsarg.txt --out /da1_data/play/inflection-points/mongo-exports/mongo_proj_metadata_R_out.csv
