bafe6d0a707d429e85211aaec0beeace8cfb6816
Noah Burgin <noah22@vols.utk.edu>
2021-01-11 16:23:04 -0500

    Commenting and argument error checking

--- scripts/filter_projects-R.sh
+++ scripts/filter_projects-R.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # usage: filter_projects.sh mongoexport_file
+#
+# Use with proj_metadata.R export format:
+# numStars,NumAuthors,projectID,EarlistCommitDate,NumCommits,LatestCommitDate
+#
 # example: filter_projects-R.sh /da1_data/play/inflection-points/mongo-exports/mongo_proj_metadata_R_out.csv 
 
-awk -F',' '($4 > 0 && $6 - $4 > 126144000) || NR==1' $1
+if [ -z "$1" ]
+then
+	echo "usage: filter_projects.sh mongoexport_file"
+	exit
+fi
+
+awk -F',' '($4 > 0 && $6 - $4 > 126144000) || NR==1' "$1"
